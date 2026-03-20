f73f0f88712f189f6e77fd5dc2523425c45a0ebd
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2026-03-20 19:47:16 +0100

    nazwa pliku

---
+++ zadanie1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv("dataset.csv")
+
+
+#ilość rekordów 
+print(df.shape)
+#sprawdzamy czy nie ma wartości Null
+print(df.isnull().sum())
+#opis danych + wszystkie zmienne
+print(df.describe())
+
